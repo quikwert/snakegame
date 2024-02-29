4ee1342a98fec7b652e4a01bd5e77d51eb68ce02
Dmitri Rodzik <dmittirodzik@gmail.com>
2024-02-29 23:13:25 +0200

    hdh

--- src/Main.java
+++ src/Main.java
@@ -2,128 +2,166 @@ import java.util.Scanner;
 
 public class Main{
     public final static int HEIGHT = 20;
-    public final static int WIDTH = HEIGHT;
-    public final static int x[] = new int[WIDTH*HEIGHT];
-    public final static int y[] = new int[HEIGHT*HEIGHT];
-    public static int bodyParts = 6;
-    public static int[] direction = new int[2];
+    public final static int[] x = new int[HEIGHT*HEIGHT];
+    public final static int[] y = new int[HEIGHT*HEIGHT];
     public static int appleX;
     public static int appleY;
-    public final static String headIcon = "@";
-    public final static String bodyIcon = "o";
-    public final static String appleIcon = "Q";
-    public static Thread thread;
-    public static Thread thread2;
 
-    public static int tickspeed = 1000/15;
+    public static int[] direction = new int[2];
+    public static int bodyParts = 6;
+    public final static String headIcon = "@";
+    public static String bodyIcon;
+    public final static String appleIcon = "Q";
+
+    public static boolean gameOn = false;
+    public static boolean loopOn;
+    public static boolean inputBlocked = false;
+
+    public static int tickSpeed = (1000/5);
+    public static int speed = 1;
+    public static Thread thread;
+
+
+
     public static void resetGame(){
+        bodyIcon = "o";
         direction[0]=1;
         direction[1]=0;
         x[0] = HEIGHT/2;
         y[0] = HEIGHT/2;
+        bodyParts = 6;
         for (int i = 1; i<bodyParts;i++){
             x[i]=x[i-1]-1;
             y[i] = y[0];
         }
         placeApple();
+        for(int i = 3; i >0;i--){
+            System.out.println(i);
+            try{
+                Thread.sleep(1000);
+            }catch (Exception e){}
+        }
+        gameOn = true;
+    }
 
+    public static void cycleGame(){
         thread = new Thread(new Runnable() {
             @Override
             public void run() {
                 for(;;){
-                    x[bodyParts] = x[bodyParts-1];
-                    y[bodyParts] = y[bodyParts-1];
-                    move();
-                    bodyParts++;
-                    checkColllision();
-                    try{
-                        new ProcessBuilder("cmd", "/c", "cls" ).inheritIO().start().waitFor();
-                    }catch(Exception e){}
 
-                    draw();
-                    try{
-                        Thread.sleep(tickspeed);
-                    }catch (Exception e){}
+                    if(gameOn){
+                        try{
+                            new ProcessBuilder("cmd", "/c", "cls" ).inheritIO().start().waitFor();
+                        }catch (Exception e){}
+                        move();
+                        inputBlocked = false;
+                        checkCollision();
+                        draw();
+                        System.out.println("l - LOOPED");
+                        System.out.println("c - CLASSIC");
+                        try{
+                            Thread.sleep(tickSpeed);
+                        }catch (Exception e){}
+                    }else{
+                        System.out.print("");
+                    }
                 }
             }
         });
-
-        thread2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Scanner sc = new Scanner(System.in);
-                for(;;){
-                    String a = sc.next();
-                    try{
-                        if(a.equals("w")&&direction[1]!=-1){
-                            direction[0]=0;
-                            direction[1]=1;
-                        }
-                        if(a.equals("d")&&direction[0]!=-1){
-                            direction[0]=1;
-                            direction[1]=0;
-                        }
-                        if(a.equals("a")&&direction[0]!=1){
-                            direction[0]=-1;
-                            direction[1]=0;
-                        }
-                        if(a.equals("s")&&direction[1]!=1){
-                            direction[0]=0;
-                            direction[1]=-1;
-                        }
-                    }catch (Exception e){}
-
-                }
-
-            }
-
-        });
-        thread2.start();
         thread.start();
+        controls();
+    }
+    public static void controls(){
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            String a = sc.next();
+            if(a.equals("w")&&direction[1]!=-1 &&!inputBlocked){
+                direction[0]=0;
+                direction[1]=1;
+                inputBlocked = true;
+            }
+            else if(a.equals("d")&&direction[0]!=-1&&!inputBlocked){
+                direction[0]=1;
+                direction[1]=0;
+                inputBlocked = true;
+            }
+            else if(a.equals("a")&&direction[0]!=1&&!inputBlocked){
+                direction[0]=-1;
+                direction[1]=0;
+                inputBlocked = true;
+            }
+            else if(a.equals("s")&&direction[1]!=1&&!inputBlocked){
+                direction[0]=0;
+                direction[1]=-1;
+                inputBlocked = true;
+            }
+            else if(a.equals("l")){
+                gameOn = false;
+                loopOn = true;
+                resetGame();
+            }
+            else if(a.equals("c")){
+                gameOn = false;
+                loopOn = false;
+                resetGame();
+            }
+            else if(a.equals("--s")){
+                speed = sc.nextInt();
+                tickSpeed = (1000/2)/speed;
+                System.out.println(speed + "   tickspeed is " + tickSpeed);
+            }
+        }
     }
     public static void move(){
         for(int i = bodyParts; i>0;i--){
             x[i] = x[i-1];
             y[i] = y[i-1];
         }
-        x[0] += direction[0];
-        y[0] -= direction[1];
+        if(loopOn){
+            x[0] = ((((x[0] + direction[0]) % HEIGHT) + HEIGHT) % HEIGHT);
+            y[0] = ((((y[0] - direction[1]) % HEIGHT) + HEIGHT) % HEIGHT);
+        }else {
+            x[0] += direction[0];
+            y[0] -= direction[1];
+        }
     }
 
-    public static void checkColllision(){
-        if(x[0]>=WIDTH||x[0]<0 || y[0]>=HEIGHT||y[0]<0){
-            System.out.println("STOP");
-            thread.stop();
+    public static void checkCollision(){
+        x[bodyParts] = x[bodyParts-1];
+        y[bodyParts] = y[bodyParts-1];
+        if(x[0]>=HEIGHT||x[0]<0 || y[0]>=HEIGHT||y[0]<0){
+            gameOn = false;
+            bodyIcon = "x";
         } else if (x[0] == appleX&&y[0]==appleY){
-
             placeApple();
             bodyParts++;
         }
-        bodyParts--;
+
         for(int i = 1; i < bodyParts; i++){
             if(x[0] == x[i]&&y[0]==y[i]){
-                System.out.println("STOP");
-                thread.stop();
+                gameOn = false;
+                bodyIcon = "x";
             }
         }
     }
     public static void placeApple(){
-       appleX = (int)Math.floor(Math.random()*HEIGHT);
-       appleY = (int)Math.floor(Math.random()*HEIGHT);
-       for(int k = 0;k<bodyParts;k++){
+        appleX = (int)Math.floor(Math.random()*HEIGHT);
+        appleY = (int)Math.floor(Math.random()*HEIGHT);
+        for(int k = 0;k<bodyParts;k++){
             if(x[k] == appleX && y[k] == appleY){
                 placeApple();
                 break;
-           }
+            }
         }
     }
     public static void draw(){
-        System.out.print("\n\n\n\n\n");
+        String image = "";
         for(int i = 0; i < HEIGHT;i++){
-            for(int j = 0; j < WIDTH;j++){
+            for(int j = 0; j < HEIGHT;j++){
                 String toPrint = ".";
                 if(j==appleX&&i==appleY) toPrint = appleIcon;
-                for(int k = 0;k<bodyParts;k++){
+                for(int k = bodyParts-1;k>=0;k--){
                     if(x[k] == j && y[k] == i){
                         toPrint = bodyIcon;
                         if(k==0){
@@ -131,13 +169,16 @@ public class Main{
                         }
                     }
                 }
-                System.out.print(toPrint + "  ");
+                image += (toPrint + "  ");
             }
-            System.out.print("\n");
+            image += "\n";
         }
+        System.out.print(image);
     }
 
     public static void main(String[] args){
-        resetGame();
+        System.out.println("l - LOOPED");
+        System.out.println("c - CLASSIC");
+        cycleGame();
     }
 }
